4da0ba3fa236998735049b97f96e6f6c4cc6351d
mrhavoc96 <tanishqsuryawanshi247@gmail.com>
2026-01-19 18:06:20 +0530

    chore: initial PERN project structure and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-12 bg-amber-300 text-white text-7xl m-4 rounded-2xl'>
+      App
+    </div>
+  )
+}
+
+export default App
