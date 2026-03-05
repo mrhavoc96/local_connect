ed252a5db6a4c5b10d36dcbc9373eefd4f3f6147
Tanishq Suryawanshi <tanishqsuryawanshi247@gmail.com>
2026-03-05 15:42:55 +0530

    fix .gitignore issue

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,11 +1,3 @@
-/** This is a higher order wrapper function,
- * 
- * it takes the input as a function (requestHanlder from 
- * healthCheck.routes.js) and returns a function as it's output
- * This function handles the  
- * 
- * NOTE: This is a general function that can be used in any of the projects. 
- * */ 
 
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
