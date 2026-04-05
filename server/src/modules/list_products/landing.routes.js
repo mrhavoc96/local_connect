f2dbd0d869c25e254d975db61bfeb9542694b31f
Tanishq Suryawanshi <tanishqsuryawanshi247@gmail.com>
2026-04-05 12:24:15 +0530

    Delete module list_products

--- server/src/modules/list_products/landing.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getLandingSuggestions } from "./landing.controller.js";
-
-const router = express.Router();
-
-router.get("/suggestions", getLandingSuggestions);
-
-export default router;
